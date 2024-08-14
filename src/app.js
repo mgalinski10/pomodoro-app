276a43d326d16037a79acf8d0327849fd7a0409f
Michał Galiński <michalg359@gmail.com>
2024-08-14 14:07:31 +0200

    timer logic task

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const start = document.querySelector(".start");
 const stop = document.querySelector(".stop");
 const finish = document.querySelector(".finish");
 
+function toggleStartStop() {
+  if (start.classList.contains("active")) {
+    start.classList.remove("active");
+    stop.classList.add("active");
+  } else {
+    stop.classList.remove("active");
+    start.classList.add("active");
+  }
+}
+
 class Timer {
   constructor(countFrom, element) {
     this.countFrom = countFrom;
@@ -10,12 +20,33 @@ class Timer {
   }
 
   setTimer() {
+    start.addEventListener("click", () => {
+      toggleStartStop();
+
+      countDown.startTimer();
+    });
     return (this.element.textContent = `${this.countFrom} : 00`);
   }
 
   startTimer() {
     let minutes = this.countFrom;
     let seconds = 0;
+
+    timer.textContent = "25 : 00";
+
+    finish.addEventListener("click", () => {
+      clearInterval(countdown);
+
+      toggleStartStop();
+
+      this.setTimer();
+    });
+
+    stop.addEventListener("click", () => {
+      var currentTime = timer.textContent;
+      clearInterval(countdown);
+      toggleStartStop();
+    });
 
     const countdown = setInterval(() => {
       if (seconds === 0) {
@@ -34,13 +65,13 @@ class Timer {
       timer.textContent = `${minutes >= 10 ? minutes : `0${minutes}`} : ${
         seconds >= 10 ? seconds : `0${seconds}`
       }`;
+      document.title = `${minutes >= 10 ? minutes : `0${minutes}`}:${
+        seconds >= 10 ? seconds : `0${seconds}`
+      } Pomodoro Timer`;
 
-      finish.addEventListener("click", () => {
-        stop.classList.remove("active");
-        start.classList.add("active");
-        clearInterval(countdown);
-        this.element.textContent = this.setTimer();
-      });
+      currentTime = `${minutes >= 10 ? minutes : `0${minutes}`} : ${
+        seconds >= 10 ? seconds : `0${seconds}`
+      }`;
     }, 1000);
   }
 }
@@ -48,13 +79,3 @@ class Timer {
 const countDown = new Timer(25, timer);
 
 countDown.setTimer();
-
-start.addEventListener("click", (e) => {
-  if (e.target.classList.contains("active")) {
-    e.target.classList.remove("active");
-    stop.classList.add("active");
-
-    timer.textContent = "25 : 00";
-    countDown.startTimer();
-  }
-});
